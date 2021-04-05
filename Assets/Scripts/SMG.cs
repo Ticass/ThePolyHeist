0a6d5a843213a0250bdb7cf3d9ced36fd497da3e
Alexis <thrifthshopdowntheroad@gmail.com>
2021-04-05 08:22:35 -0400

    :sparkles: bullet Hit effect

--- Assets/Scripts/SMG.cs
+++ Assets/Scripts/SMG.cs
@@ -6,6 +6,8 @@ public class SMG : MonoBehaviour
 {
     [Header("Gun FX")]
     public ParticleSystem GunShot;
+    public ParticleSystem GunShell;
+    public GameObject HitParticle;
     public AudioSource GunSound;
 
     [Header("Gun Settings")]
@@ -13,12 +15,15 @@ public class SMG : MonoBehaviour
     public float range = 100f;
     public Transform ShootPos;
 
+    void Start(){
+        GunShell.Stop();
+    }
+
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0)){
             Shoot();
-            GunShot.Play();
         }
     }
 
@@ -27,13 +32,16 @@ public class SMG : MonoBehaviour
     void Shoot(){
         GunShot.Play();
         RaycastHit hit;
-        if (Physics.Raycast(ShootPos.transform.position, ShootPos.transform.forward, out hit, range)){
+        if (Physics.Raycast(ShootPos.transform.position, ShootPos.transform.forward, out hit, range))
+        Instantiate(HitParticle, hit.point, Quaternion.LookRotation(hit.normal));
             Debug.Log(hit.transform.name);
 
             Target target = hit.transform.GetComponent<Target>();
             if (target != null){
                 target.TakeDamage(10);
             }
+
+            
         }
-    }
 }
+
